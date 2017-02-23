b4d1808821e213f403b6ab67d089322dd52d27d2
Nikoline Visholm <nikolinevisholm@laptop-kga43er8.kea.local>
2017-02-23 12:47:52 +0100

    6. iteration

---
+++ js/petrograd.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("siden vises");
+
+    //læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
+    //visProdukt();
+}
+
+
+function visProduktListe(listen) {
+    console.table (listen);
+    listen.forEach(visProdukt);
+}
+
+function visProdukt(produkt) {
+    console.log(produkt);
+    // klon produkt_template
+    var klon = document.querySelector("#produkt_template").content.cloneNode(true);
+
+    // indsæt data i klon
+
+    // append klon til .produkt_liste
+    document.querySelector(".produktliste").appendChild(klon);
+}
